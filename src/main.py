20bcae126f4c883fed98f474abb9fc9db11f3abc
Wodiin <samphillips246@gmail.com>
2026-05-03 22:55:18 +1000

    created a copy folder generator in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINKS, "https://www.boot.dev")
-    print(text_node)
+    create_folder("public")
+    copy_folder_and_files("static", "public")
 
+
+def create_folder(folder_name):
+    if os.path.exists(folder_name):
+        shutil.rmtree(folder_name)
+    os.makedirs(folder_name)
+
+def copy_folder_and_files(src, dest):
+    os.makedirs(dest, exist_ok=True)
+    for item in os.listdir(src):
+        path = os.path.join(src, item)
+        if os.path.isfile(path):
+            shutil.copy(path, dest)
+        else:
+            copy_folder_and_files(path, os.path.join(dest, item))
+    
 
 
 if __name__ == "__main__":
